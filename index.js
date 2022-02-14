5431eb2a6c52e34a637a1006a51fa7c0ce759960
alepri51 <39708925+alepri51@users.noreply.github.com>
2022-02-14 14:26:20 +0300

    ref: only pub sub

--- index.js
+++ index.js
@@ -1,124 +1,129 @@
-const sseSubscribe = (req, res, onClose) => {
-    //req.socket.setTimeout(0);
-    //res.connection.setTimeout(0);
-    req.on('close', () => {
-        onClose && onClose(req);
-    });
-
-    res.writeHead(200, {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive',
-        'X-Accel-Buffering': 'no',
-        'Content-Encoding': 'no'
-    });
-
-    res.write('\n');
-    res.write(`retry: 3000\n\n`);
-
-};
-
-const ssePublish = (res, event, data) => {
-    event && res.write(`event: ${event}\n`);
-    res.write(`data: ${data || ''}\n\n`);
-};
-
-
-class SSE {
-    constructor({ pub, sub } = {}) {
-        this.error = void 0;
-        this.ready = false;
-
-        this.clients = {};
-
-        const send = (channel, message, clients) => {
-            let { event, data } = JSON.parse(message);
-                    
-            const publish = clients[channel];
-            
-            data = typeof(data) === 'object' ? JSON.stringify(data) : data;
-
-            publish && publish.internal.forEach(internal => {
-                internal(event, data)
-            }); //publish.internal(event, data);
-        }
-
+class SSEPool {
+    constructor({ pub, sub }) {
         if(pub && sub) {
-            this.sub = sub;
             this.pub = pub;
+            this.sub = sub;
 
-            this.sub.on('message', (channel, message) => {
-                send(channel, message, this.clients);
-            });
+            this.pool = {};
+            this.sse_map = new WeakMap();
         }
         else {
-            const self = this;
-
-            this.pub = {
-                publish(channel, message) {
-                    send(channel, message, self.clients);
-                }
-            };
-
-            this.sub = {
-                subscribe(channel) {
-
-                },
-                unsubscribe(channel) {
-
-                }
-            };
+            throw { code: 422, message: 'Missing params.' };
         }
     }
 
+    connect(req, res, channels) {
+        const { pub, sub } = this;
+        const sse = new SSE({ pub, sub, pool: this });
 
-    subscribe(req, res, channel) {
-        const subscribe = (req, res) => {
-            //const interval = setInterval(() => this.clients[channel] && this.clients[channel](void 0, channel), 10000);
+        const start = sse.start(req, res, channels);
 
-            sseSubscribe(req, res, (req) => {
-                //onClose
-                if(this.clients[channel]) {
-                    let external = this.clients[channel];
-                    external.internal.delete(req);
+        return start;
+    }
 
-                    if(!external.internal.size) {
-                        this.clients[channel] = void 0;
+    disconnect(sse) {
+    }
 
-                        //clearInterval(interval);
-
-                        this.sub.unsubscribe(channel);
-
-                        res.end();
-                    }
-                    
-                }
-            });
-
-            return (event, data) => {
-                ssePublish(res, event, data);
-            }
-        }
-
-        let external = this.clients[channel];
-
-        if(!external) {
-            external = (event, data) => {
-                this.pub.publish(channel, JSON.stringify({ event, data }));
-            };
-        }
-        
-        external.internal = external.internal || new Map();
-        external.internal.set(req, subscribe(req, res));
-        /* external.internal = external.internal || [];
-        external.internal = [...external.internal, subscribe(req, res)]; */
-
-        this.clients[channel] = external; //publish to client
-
-        this.sub.subscribe(channel);
-
-        return external;
+    publish({ channel, event, data }) {
+        return this.pub.publish(`${process.env.NAMESPACE}:${channel}`, JSON.stringify({ event, data }));
     }
 }
 
-module.exports = (pub, sub) => new SSE(pub, sub);
+class SSE {
+    constructor({ pub, sub, pool }) {
+        if(pub && sub) {
+            this.pub = pub;
+            this.sub = sub;
+
+            this.pool = pool;
+
+            this.started = false;
+        }
+        else {
+            throw { code: 422, message: 'Missing params.' };
+        }
+    }
+
+    async start(req, res, channels) {
+        if(!this.started) {
+            this.stoped = false;
+
+            if(req && res) {
+                this.req = req;
+                this.res = res;
+
+                this.started = true;
+
+                res.writeHead(200, {
+                    'Content-Type': 'text/event-stream',
+                    'Cache-Control': 'no-cache, no-transform',
+                    'Connection': 'keep-alive',
+                    'X-Accel-Buffering': 'no',
+                    'Content-Encoding': 'no'
+                });
+            
+                res.write('\n');
+                res.write(`retry: 3000\n\n`);
+
+                res.on('close', (...ags) => {
+                    this.stoped = true;
+                });
+
+                res.on('end', (...ags) => {
+                    this.stoped = true;
+                });
+
+                res.on('error', (...ags) => {
+                    this.stoped = true;
+                });
+                
+                res.on('aborted', (...ags) => {
+                    this.stoped = true;
+                });
+
+                res.socket.on('close', function () {
+                    this.stoped = true;
+                });
+            }
+
+            channels = Array.isArray(channels) ? channels : [channels];
+
+            for(const channel of channels) {
+                const namespace_channel = `${process.env.NAMESPACE}:${channel}`;
+    
+                this.sub.subscribe(namespace_channel);
+            }
+
+            this.sub.on('message', (channel, message) => {
+                const { event, data } = JSON.parse(message);
+
+                event && this.res.write(`event: ${channel}:${event}\n`);
+                this.res.write(`data: ${JSON.stringify(data) || ''}\n`);
+                this.res.write(`id: ${Date.now()}\n\n`);
+            });
+
+            return new Promise(async (resolve, reject) => {
+                while(!this.stoped) {
+                    await new Promise((resolve) => {
+                        setTimeout(() => {
+                            this.res.write(`event: HEALTH\n`);
+                            this.res.write(`data: ${Date.now()}\n`);
+                            this.res.write(`id: ${Date.now()}\n\n`);
+                            this.res.write(`retry: 10000\n\n`);
+                            
+                            resolve()
+                        }, 15000);
+                    })
+                }
+
+                reject({ code: 400, message: 'SSE client disconnected.' });
+            });
+        }
+    }
+
+    stop() {
+        this.stoped = true;
+    }
+}
+
+module.exports = SSEPool;
